8c73b400c09f820c83bfa250962fce71076a1e18
Ramiro <ramiroroman306@gmail.com>
2025-10-16 15:37:34 -0300

    Correccion de las rutas

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -4,6 +4,6 @@ import { login, register, logout } from "../controllers/auth.controllers.js";
 
 export const authRoutes = Router();
 
-authRoute.post("/auth/register", register);
-authRoute.post("/auth/login", login);
+authRoutes.post("/auth/register", register);
+authRoutes.post("/auth/login", login);
 authRoutes.post("/auth/logout", logout);
